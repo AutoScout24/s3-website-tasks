344f4d2bf601f594a75512c2f64038631f414978
alexlawrence <mail@alex-lawrence.com>
2018-02-14 17:00:49 +0100

    create-or-update-stack: Add aws region parameter with default value

--- src/create-or-update-stack/index.js
+++ src/create-or-update-stack/index.js
@@ -1,19 +1,19 @@
 const aws = require('aws-sdk');
 const promisify = require('util.promisify');
 
-const cloudFormation = new aws.CloudFormation();
-
-const describeStacksAsync = promisify(cloudFormation.describeStacks.bind(cloudFormation));
-const createStackAsync = promisify(cloudFormation.createStack.bind(cloudFormation));
-const updateStackAsync = promisify(cloudFormation.updateStack.bind(cloudFormation));
-
 const waitForStackCompletion = require('./wait-for-stack-completion');
 
-const determineStackOperation = stackName => describeStacksAsync({StackName: stackName})
-.then(() => updateStackAsync)
-.catch(() => createStackAsync);
+module.exports = ({stackName, cloudFormationTemplate, region = 'eu-west-1'}) => {
+  const cloudFormation = new aws.CloudFormation({region});
 
-module.exports = ({stackName, cloudFormationTemplate}) => {
+  const describeStacksAsync = promisify(cloudFormation.describeStacks.bind(cloudFormation));
+  const createStackAsync = promisify(cloudFormation.createStack.bind(cloudFormation));
+  const updateStackAsync = promisify(cloudFormation.updateStack.bind(cloudFormation));
+
+  const determineStackOperation = stackName => describeStacksAsync({StackName: stackName})
+  .then(() => updateStackAsync)
+  .catch(() => createStackAsync);
+
   const startOfStackChanges = Date.now();
   return determineStackOperation(stackName)
   .then(createOrUpdateStackAsync => createOrUpdateStackAsync({

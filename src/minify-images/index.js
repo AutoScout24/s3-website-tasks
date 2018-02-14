facb6e94fc7187c95c0315556d4b0f26c60809ca
alexlawrence <mail@alex-lawrence.com>
2018-02-14 15:46:59 +0100

    minify images: Fix race conditions by execute minification steps in sequence

--- src/minify-images/index.js
+++ src/minify-images/index.js
@@ -13,17 +13,22 @@ const executeImagemin = (inputDirectory, outputDirectory, plugins) => imagemin(
   [`${inputDirectory}/*.jpg`], outputDirectory, {plugins}
 );
 
+const executeInSequence = (list, asyncOperation) => list.reduce(
+  (previousOperation, listItem) => (previousOperation.then(asyncOperation.bind(null, listItem))),
+  Promise.resolve()
+);
+
 const createFallbackWebpFiles = require('./create-fallback-webp-files');
 
 module.exports = ({srcPath, destPath, quality = 70}) => rimrafAsync(destPath)
 .then(() => globAsync(srcPath + '/**/*/'))
 .then(subdirectories => subdirectories.concat([srcPath]))
-.then(subdirectories => {
-  return Promise.all(subdirectories.map(
-    subdirectory => executeImagemin(subdirectory, subdirectory.replace(srcPath, destPath), [imageminWebp(quality)])
-  ))
-  .then(() => Promise.all(subdirectories.map(
-    subdirectory => executeImagemin(subdirectory, subdirectory.replace(srcPath, destPath), [imageminMozjpeg(quality)])
-  )));
-})
+.then(subdirectories => executeInSequence(
+  subdirectories,
+  subdirectory => executeImagemin(subdirectory, subdirectory.replace(srcPath, destPath), [imageminMozjpeg(quality)])
+).then(() => subdirectories))
+.then(subdirectories => executeInSequence(
+  subdirectories,
+  subdirectory => executeImagemin(subdirectory, subdirectory.replace(srcPath, destPath), [imageminWebp(quality)])
+))
 .then(createFallbackWebpFiles.bind(null, destPath));

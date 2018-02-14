fb8d7fe66eae5ad38aba1a8bfb21cd52b29b5c36
alexlawrence <mail@alex-lawrence.com>
2018-02-14 16:49:30 +0100

    minify images: inline fs.copyFile() call

--- src/minify-images/create-fallback-webp-files.js
+++ src/minify-images/create-fallback-webp-files.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const glob = require('glob');
 const promisify = require('util.promisify');
 
-const copyFileAsync = promisify(fs.copyFile.bind(fs));
 const globAsync = promisify(glob);
 
 module.exports = (folder) => Promise.all([
@@ -14,5 +13,9 @@ module.exports = (folder) => Promise.all([
   jpegFile => webpFiles.indexOf(jpegFile.replace('.jpg', '.webp')) == -1
 ))
 .then(jpegFilesWithoutAccordingWebp => Promise.all(jpegFilesWithoutAccordingWebp.map(
-  jpegFile => copyFileAsync(jpegFile, jpegFile.replace('.jpg', '.webp'))
+  jpegFile => new Promise(
+    (resolve, reject) => fs.copyFile(
+      jpegFile, jpegFile.replace('.jpg', '.webp'), error => error ? reject(error) : resolve()
+    )
+  )
 )));

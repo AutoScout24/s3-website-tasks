b45fe3f91d6e0a717ca2316691b74aeb7ebd8ff4
Natalya Gunn <natalya.gunn@scout24.com>
2018-05-07 14:04:31 +0200

    revert

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ module.exports = {
   checkForInternalDeadLinks: require('./check-for-internal-dead-links'),
   createDeadLinksReport: require('./check-for-internal-dead-links/create-report'),
   createOrUpdateStack: require('./create-or-update-stack'),
-  deleteStack: require('./delete-stack'),
   minifyImages: require('./minify-images'),
   createCustomRedirectDefinitions: require('./create-custom-redirect-definitions'),
   createTrailingSlashRedirectDefinitions: require('./create-trailing-slash-redirect-definitions')

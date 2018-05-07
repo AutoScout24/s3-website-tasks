44e70727b9320d46c6414c31b750bd41675e1ffa
Natalya Gunn <natalya.gunn@scout24.com>
2018-05-07 13:51:05 +0200

    added delete stack code

--- src/delete-stack/index.js
+++ src/delete-stack/index.js
@@ -1,7 +1,7 @@
 const aws = require('aws-sdk');
 const promisify = require('util.promisify');
 
-module.exports = ({stackName, cloudFormationTemplate, region = 'eu-west-1'}) => {
+module.exports = ({stackName, region = 'eu-west-1'}) => {
   const cloudFormation = new aws.CloudFormation({region});
   const deleteStacksAsync = promisify(cloudFormation.deleteStack.bind(cloudFormation));
   const deleteStack = stackName => deleteStacksAsync({StackName: stackName});
